7834dd79706e28d27587b170c3be96d58735c8f9
HappyRehund <rayhanardian045@gmail.com>
2026-05-18 17:22:35 +0700

    feat: Refactor SeismicSectionService to remove unused methods and enhance section range retrieval

--- services/seismic_section_service.py
+++ services/seismic_section_service.py
@@ -1,5 +1,5 @@
 from repositories.seismic_section_repository import SeismicSectionRepository
-from models.seismic_section_model import SeismicSection, SectionType
+from models.seismic_section_model import SeismicSection, SectionType, SeismicRangesResponse
 from typing import Optional, List
 
 
@@ -13,14 +13,12 @@ class SeismicSectionService:
     def get_crossline_image(self, number: int, dataset: str = 'default') -> Optional[SeismicSection]:
         return self.repository.find_by_number(SectionType.CROSSLINE, number, dataset=dataset)
 
-    def get_inline_mjb_image(self, number: int, dataset: str = 'default') -> Optional[SeismicSection]:
-        return self.repository.find_by_number(SectionType.INLINEMJB, number, dataset=dataset)
-
-    def get_crossline_mjb_image(self, number: int, dataset: str = 'default') -> Optional[SeismicSection]:
-        return self.repository.find_by_number(SectionType.CROSSLINEMJB, number, dataset=dataset)
-
     def get_available_datasets(self) -> List[str]:
         return self.repository.list_datasets()
 
     def get_section_types(self, dataset: str) -> List[str]:
         return self.repository.list_section_types(dataset)
+
+    def get_section_ranges(self, dataset: str = 'default') -> SeismicRangesResponse:
+        ranges = self.repository.get_section_ranges(dataset=dataset)
+        return SeismicRangesResponse(dataset=dataset, ranges=ranges)
